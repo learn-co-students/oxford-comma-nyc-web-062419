8fff664d113505fafcdd6f0246cb07dc4500d68b
ghatzid <ghatzid@gmail.com>
2019-06-22 10:11:08 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        return array.join
+    elsif array.length == 2
+        return array.join(" and ")
+    else
+        newstring = array.join(", ") 
+        newstring.sub("#{array.last}","and #{array.last}")
+    end
 end
+
